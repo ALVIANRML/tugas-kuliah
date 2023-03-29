24ae2b4ef31a885e62bf8e7e434f743c37e763b9
ALVIAN_085 <114425441+ALVIANRML@users.noreply.github.com>
2023-03-30 02:34:32 +0700

    Add files via upload

---
+++ tugas_no1c.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+
+    int i=2;
+    int k=7;
+    int l=1;
+    int c=9;
+    int u=5;
+    int s=6;
+    int hasil;
+
+    hasil = i-9 > k || l+3 < 4*c;
+    cout << "hasil dari hasil = i-9 > k or l+3 < 4*c adalah "<< hasil;
+    return 0;
+}
